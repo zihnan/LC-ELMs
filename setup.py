7d376805d583a5cd3c079ff1a54f5742eff750ed
zihnan <zwillis007@gmail.com>
2017-06-29 14:31:33 +0800

    add download url into setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     author = 'lin,zihnan',
     author_email = 'zwillis007@gmail.com',
     url = 'https://github.com/zihnan/LC-ELMs',
-    download_url = '',
+    download_url = 'https://github.com/zihnan/LC-ELMs/tarball/v1.0',
     keywords = ['ELM', 'ensemble learning algorithm', 'Lc-ELMs'],
     classifiers = [],
 )
