27b8f99d7e811cefdde4c8a150d73e04b094dfb3
zihnan <zwillis007@gmail.com>
2017-06-29 14:37:53 +0800

    pre-release

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     scripts = [],
     version = '1.0',
     description = 'A Linear Combination Model-Based Ensemble Extreme Learning Machine',
-    author = 'lin,zihnan',
+    author = 'lin, zihnan',
     author_email = 'zwillis007@gmail.com',
     url = 'https://github.com/zihnan/LC-ELMs',
     download_url = 'https://github.com/zihnan/LC-ELMs/tarball/v1.0',
