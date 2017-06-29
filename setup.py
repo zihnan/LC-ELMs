a2b4c41f9431409ac1fc3a8bbe6593562db11b01
zihnan <zwillis007@gmail.com>
2017-06-29 14:12:18 +0800

    fixed setup.py

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ setup(
     description = 'A Linear Combination Model-Based Ensemble Extreme Learning Machine',
     author = 'lin,zihnan',
     author_email = 'zwillis007@gmail.com',
-    url = 'https://github.com/dokelung/MyProject',
-    download_url = 'https://github.com/dokelung/MyProject/tarball/v1.0',
+    url = 'https://github.com/zihnan/LC-ELMs',
+    download_url = '',
     keywords = ['ELM', 'ensemble learning algorithm', 'Lc-ELMs'],
     classifiers = [],
 )
