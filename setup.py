c206fc93348ee39d30884c777aa634f01876c43e
ZihNan station <zihnan@ntu.edu.tw>
2017-06-29 10:04:05 +0800

    1st test

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name = 'lcelm',
+    packages = ['lc-elm'],
+    scripts = [],
+    version = '1.0',
+    description = 'A Linear Combination Model-Based Ensemble Extreme Learning Machine',
+    author = 'lin,zihnan',
+    author_email = 'zwillis007@gmail.com',
+    url = 'https://github.com/dokelung/MyProject',
+    download_url = 'https://github.com/dokelung/MyProject/tarball/v1.0',
+    keywords = ['ELM', 'ensemble learning algorithm', 'Lc-ELMs'],
+    classifiers = [],
+)
+
